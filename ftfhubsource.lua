0161dff85f5f3c7b213e79510206317fdf87db82
Ewerton99BRYT99 <ewerton99backs@gmail.com>
2025-04-09 12:57:24 -0300

    Update ftfhubsource.lua

--- ftfhubsource.lua
+++ ftfhubsource.lua
@@ -79,7 +79,7 @@ Section2:NewToggle("OutlineESP", "ToggleInfo", function(state)
         outlinetoggle = true
         outlineESP()
     else
-        outlineESP = false
+        outlinetoggle = false
 	outlineESP()
     end
 end)
